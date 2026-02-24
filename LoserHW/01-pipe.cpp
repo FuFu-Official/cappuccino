82b64980ad436fe9c4ccb86d2a8afee01a93fb4a
fufu <fuhongrui15@gmail.com>
2026-02-25 00:18:00 +0800

    lhw: 01 finished

--- LoserHW/01-pipe.cpp
+++ LoserHW/01-pipe.cpp
@@ -1,10 +1,22 @@
+// Pipe operator for vector
+// c++20
+#include <concepts>
 #include <functional>
 #include <iostream>
 #include <vector>
+
+template <typename U, typename F>
+  requires std::regular_invocable<F, U &>
+std::vector<U> &operator|(std::vector<U> &v1, F f) {
+  for (auto &i : v1) {
+    f(i);
+  }
+  return v1;
+}
 
 int main() {
   std::vector v{1, 2, 3};
   std::function f{[](const int &i) { std::cout << i << ' '; }};
   auto f2 = [](int &i) { i *= i; };
-  // v | f2 | f;
+  v | f2 | f;
 }
--- LoserHW/README.md
+++ LoserHW/README.md
@@ -1,1 +1,3 @@
+## References
+
 [LoserHomework](https://github.com/Mq-b/Loser-HomeWork/tree/main)
